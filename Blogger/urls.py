d49f45a5c99fe4ef21cae179d340ab2cd5fcb8fd
Vipin Chaudhary <vipinchaudhary31122002@gmail.com>
2022-09-03 00:14:55 +0530

    created the homepage of the website

--- Blogger/urls.py
+++ Blogger/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
+from .views import HomeView
 
 urlpatterns = [
-    path("Verification/",include('Verification.urls'))
-    
+    path('', HomeView, name='Home'),
+    # path('Verification/', include('django.contrib.auth.urls')),
+    # path("Verification/",include('Verification.urls'))
+
 ]
